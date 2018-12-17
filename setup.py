b1e67bab08b1a05a5b6bbc40e54a1bb3581dc6b4
David Mulder <dmulder@suse.com>
2018-12-17 07:57:06 -0700

    Create a setup file and move source to module dir

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='adcommon',
+    version='1.0',
+    author='David Mulder',
+    author_email='dmulder@suse.com',
+    description='Common code for the yast python ad modules',
+    url='https://github.com/dmulder/yast2-adcommon-python',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 OR LATER (GPLV3+)',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
